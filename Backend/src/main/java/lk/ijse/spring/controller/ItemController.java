79a9abb22b6459f1c13ba7163f7c1921cb785fe1
HelithaSri <helitha.pravin@gmail.com>
2022-06-26 11:47:45 +0530

    add item and get item done

--- Backend/src/main/java/lk/ijse/spring/controller/ItemController.java
+++ Backend/src/main/java/lk/ijse/spring/controller/ItemController.java
@@ -1,11 +1,36 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.ItemDTO;
+import lk.ijse.spring.service.ItemService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
 /**
  * @author Helitha Sri
  * @created 6/25/2022 - 2:10 AM
  * @project Spring POS
  */
 
+@RestController
+@RequestMapping("api/item")
+@CrossOrigin
 public class ItemController {
+
+    @Autowired
+    ItemService itemService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllItems(){
+        return new ResponseUtil(200,"OK",itemService.getAllItems());
+    }
+
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil addItem(ItemDTO itemDTO){
+        itemService.saveItem(itemDTO);
+        return new ResponseUtil(200,"Item added Successfully",null);
+    }
 
 }
