796d07c67ca855b530518fdcae2ad24608a29f71
HelithaSri <helitha.pravin@gmail.com>
2022-07-03 01:32:08 +0530

    Added Exception Handler

---
+++ Backend/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
@@ -0,0 +1,26 @@
+package lk.ijse.spring.advisor;
+
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Helitha Sri
+ * @created 7/3/2022 - 1:28 AM
+ * @project Spring POS
+ */
+
+@RestController
+@CrossOrigin
+public class AppWideExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({Exception.class})
+    public ResponseUtil exceptionHandler(Exception e){
+        return new ResponseUtil(500, e.getMessage(),null);
+    }
+
+}
